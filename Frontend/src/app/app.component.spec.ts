585277fa6ae0f2bec7f0c2469bbf2945cb7406b8
BJSwaroop <bjswaroop1@gmail.com>
2023-02-25 18:49:51 +0530

    bug fix

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Frontend'`, () => {
+  it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Frontend');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
   });
 });
